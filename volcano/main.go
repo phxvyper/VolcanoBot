2ecb881bbd92658b42f8a6dd88a0db7cad511ebc
Tristen Horton <tristen@tristenhorton.com>
2016-08-13 19:50:15 -0700

    added printDebug and refactored because GOLANG

--- volcano/main.go
+++ volcano/main.go
@@ -7,8 +7,10 @@ import (
 	"flag"
 	"strings"
 	"strconv"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/daviddengcn/go-colortext"
 )
 
 type CommandFunction func(*discordgo.Session, *discordgo.MessageCreate, string, []string) error;
@@ -22,22 +24,48 @@ type Command struct {
 	calledFunc CommandFunction
 };
 
-var uname string;
-var pword string;
-var cmdPrefix string;
+var (
+	commands []Command;
 
-var commands []Command;
+	uname string;
+	pword string;
+	cmdPrefix string;
+	debug bool;
+
+	botname = "MemesToGO";
+
+	invalid_args = errors.New("invalid args");
+
+	kernel32, _ = syscall.LoadLibrary("kernel32.dll");
+	getModuleHandle, _ = syscall.GetProcAddress(kernel32, "GetModuleHandleW");
+
+	user32, _ = syscall.LoadLibrary("user32.dll");
+	messageBox, _ = syscall.GetProcAddress(user32, "MessageBoxW");
+)
 
 func init() {
 	flag.StringVar(&uname, "u", "", "Account Username");
 	flag.StringVar(&pword, "p", "", "Account Password");
 	flag.StringVar(&cmdPrefix, "f", ">", "Command Prefix");
+	flag.BoolVar(&debug, "d", false, "Debug Mode");
+	
+	flag.StringVar(&uname, "username", "", "Account Username");
+	flag.StringVar(&pword, "password", "", "Account Username");
+	flag.StringVar(&cmdPrefix, "prefix", "", "Account Username");
+	flag.BoolVar(&debug, "debug", false, "Debug Mode");
+
 	flag.Parse();
 
 	registerCommands();
 }
 
 func main() {
+
+	defer syscall.FreeLibrary(kernel32);
+	defer syscall.FreeLibrary(user32);
+
+	ct.Foreground(ct.White, true);
+
 	if (uname == "" || pword == "") {
 		fmt.Println("No user info provided. Please run: volcano -u <email> -p <password>");
 		return;
@@ -64,6 +92,13 @@ func main() {
 }
 
 func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
+
+	if (strings.ToLower(message.Author.Username) == strings.ToLower(botname)) {
+		return;
+	}
+
+	printDebug("New message: " + message.Content);
+
 	if (strings.HasPrefix(message.Content, cmdPrefix)) {
 
 		cmd, args := spliceCommand(message.Content);
@@ -76,13 +111,13 @@ func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate)
 					return;
 				}
 
+				session.ChannelMessageDelete(message.ChannelID, message.ID);
+
 				err := element.calledFunc(session, message, cmd, args);
 
 				if (err != nil) {
-					if (err.Error() == "invalid args") {
-						//session.ChannelMessageSend(message.ChannelID, strcat("Invalid arguments passed\n", element.help));
-						session.ChannelMessageSend(message.ChannelID, strcat("Invalid arguments passed\n"));
-						printHelp(session, message, element);
+					if (err == invalid_args) {
+						printHelp(session, message, element, "Invalid arguments passed.");
 					} else {
 						session.ChannelMessageSend(message.ChannelID, err.Error());
 					}
@@ -121,10 +156,9 @@ func spliceCommand(cmd string) (string, []string) {
 	inDelim := false;
 	currentArg := 0;
 
-	for i := 0; i < len(cmd); i++ {
-		ch := cmd[i];
+	for _,char := range cmd {
 
-		if (ch == '"') {
+		if (char == '"') {
 			if (inDelim) {
 				currentArg++;
 				inDelim = false;
@@ -132,14 +166,14 @@ func spliceCommand(cmd string) (string, []string) {
 				inDelim = true;
 				continue;
 			}
-		} else if (ch == ' ' && !inDelim) {
+		} else if (char == ' ' && !inDelim) {
 			currentArg++;
 		}
 
 		if (currentArg + 1 > len(args)) {
 			args = append(args, "");
 		} else {
-			args[currentArg] = strcat(args[currentArg], string(ch));
+			args[currentArg] = strcat(args[currentArg], string(char));
 		}
 	}
 
@@ -197,7 +231,7 @@ func userCanUseCommand(session *discordgo.Session, message *discordgo.MessageCre
 	return false;
 }
 
-func printHelp(session *discordgo.Session, message *discordgo.MessageCreate, cmd Command) {
+func printHelp(session *discordgo.Session, message *discordgo.MessageCreate, cmd Command, reason string) {
 
 	aliases := "";
 	requires := "";
@@ -221,11 +255,21 @@ func printHelp(session *discordgo.Session, message *discordgo.MessageCreate, cmd
 		requires = "\nrequires: " + cmd.role + " or higher\n"
 	}
 
-	session.ChannelMessageSend(message.ChannelID, strcat("\n```xl\n", cmdPrefix, cmd.prefix, " ", cmd.help, "\n", aliases, "-    ", cmd.description, requires, "```"));
+	session.ChannelMessageSend(message.ChannelID, strcat(reason, "\n```xl\n", cmdPrefix, cmd.prefix, " ", cmd.help, "\n", aliases, "-    ", cmd.description, requires, "```"));
 }
 
 // Command generation
+/*
+prefix = the main command
+aliases = a string array of alternate commands that do the same thing
+description = what the command does
+help = all of the arguments. [] = optional, <> = required
+role = the minimum required role to use this command. Blank = no requirement
+func = the function called
+*/
 func createCommand(prefixStr string, aliasList []string, description string, help string, role string, callFunc CommandFunction) error {
+
+	printDebug("Registering command: " + prefixStr);
 
 	pref := strings.ToLower(prefixStr);
 	var aliases []string;
@@ -237,11 +281,17 @@ func createCommand(prefixStr string, aliasList []string, description string, hel
 	for _,cmd := range commands {
 
 		if (pref == cmd.prefix || stringInSlice(pref, cmd.aliases)) {
-			return errors.New(strcat("There is already a command with the prefix or alias '", pref, "'"));
+
+			printDebug("There is already a command with the prefix or alias '" + pref + "'");
+
+			return errors.New("There is already a command with the prefix or alias '" + pref + "'");
 		}
 
 		for _,alias := range aliases {
 			if (alias == cmd.prefix || stringInSlice(alias, cmd.aliases)) {
+
+				printDebug("There is already a command with the prefix or alias '" + alias + "'");
+
 				return errors.New(strcat("There is already a command with the prefix or alias '", alias, "'"));
 			}
 		}
@@ -252,62 +302,25 @@ func createCommand(prefixStr string, aliasList []string, description string, hel
 	return nil;
 }
 
+func printDebug(text string) {
+	if (debug) {
+		ct.ChangeColor(ct.Yellow, true, ct.Black, false);
+		fmt.Println("[debug] " + text + "\n");	
+		ct.ResetColor();
+	}
+}
+
 // Register commands here
 func registerCommands() {
-	createCommand("test", nil, "prints \"testing!\"", "", "", testCommandFunction);
 
-	/*
-	prefix = the main command
-	aliases = a string array of alternate commands that do the same thing
-	description = what the command does
-	help = all of the arguments. [] = optional, <> = required
-	role = the minimum required role to use this command. Blank = no requirement
-	func = the function called
-	*/
-	createCommand(
-		"help",
-		[]string{"?"},
-		"prints help info provided by a command, or lists all commands.",
-		"[cmd]",
-		"",
-		helpCommand);
-}
+	printDebug("Registering commands");
 
-// Command functions go down here (events that occur when a function is called)
-func helpCommand(session *discordgo.Session, message *discordgo.MessageCreate, cmd string, args []string) error {
+	registerHelp();
+	registerStrokeOrder();
 
-	if (len(args) < 1) {
-		info := "\n```xl\nCommands:\n";
-
-		for _,item := range commands {
-			info += strcat("	", item.prefix, " ", item.help, ": ", item.description, "\n");
-		}
-
-		info += "\n```";
-
-		session.ChannelMessageSend(message.ChannelID, info);
-
-		return nil;
-
-	} else {
-		for _,item := range commands {
-			if (item.prefix == strings.ToLower(args[0]) || stringInSlice(strings.ToLower(args[0]), item.aliases)) {
-				//session.ChannelMessageSend(message.ChannelID, strcat(args[0], " help:\n", item.help));
-
-				printHelp(session, message, item);
-
-				return nil;
-			}
-		}
+	if (debug) {
+		registerTest();	
 	}
-
-	return nil;
-}
-
-func testCommandFunction(session *discordgo.Session, message *discordgo.MessageCreate, cmd string, args []string) error {
-	session.ChannelMessageSend(message.ChannelID, "testing!");
-
-	return nil;
 }
 
 func showUserPermissions(session *discordgo.Session, message *discordgo.MessageCreate, cmd string, args []string) error {
