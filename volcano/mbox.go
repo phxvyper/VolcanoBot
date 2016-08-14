b2b6e80319e20ee1db4586f5c47468188d7d755c
Tristen Horton <tristen@tristenhorton.com>
2016-08-13 19:48:24 -0700

    windows message box calls for debugging purposes

---
+++ volcano/mbox.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+const (
+	MB_OK                = 0x00000000;
+	MB_OKCANCEL          = 0x00000001;
+	MB_ABORTRETRYIGNORE  = 0x00000002;
+	MB_YESNOCANCEL       = 0x00000003;
+	MB_YESNO             = 0x00000004;
+	MB_RETRYCANCEL       = 0x00000005;
+	MB_CANCELTRYCONTINUE = 0x00000006;
+	MB_ICONHAND          = 0x00000010;
+	MB_ICONQUESTION      = 0x00000020;
+	MB_ICONEXCLAMATION   = 0x00000030;
+	MB_ICONASTERISK      = 0x00000040;
+	MB_USERICON          = 0x00000080;
+	MB_ICONWARNING       = MB_ICONEXCLAMATION;
+	MB_ICONERROR         = MB_ICONHAND;
+	MB_ICONINFORMATION   = MB_ICONASTERISK;
+	MB_ICONSTOP          = MB_ICONHAND;
+
+	MB_DEFBUTTON1 = 0x00000000;
+	MB_DEFBUTTON2 = 0x00000100;
+	MB_DEFBUTTON3 = 0x00000200;
+	MB_DEFBUTTON4 = 0x00000300;
+)
+
+func abort(funcname string, err error) {
+	panic(fmt.Sprintf("%s failed: %v", funcname, err));
+}
+
+func showMessageBox(caption string, text string, style uintptr) (result int) {
+	var nargs uintptr = 4;
+
+	ret, _, callerr := syscall.Syscall9(uintptr(messageBox),
+		nargs,
+		0,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(text))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(caption))),
+		style,
+		0,
+		0,
+		0,
+		0,
+		0);
+
+	if (callerr != 0) {
+		abort("Call MessageBox", callerr);
+	}
+
+	result = int(ret);
+	return;
+}
